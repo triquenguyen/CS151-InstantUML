d7947d7e78ca60ef41fef7fd9b0089caee37516e
Trique Nguyen <ngngoctridung123@gmail.com>
2023-05-04 14:19:54 -0700

    finished diagram

--- src/main/java/ui/UMLDiagramPanel.java
+++ src/main/java/ui/UMLDiagramPanel.java
@@ -1,9 +1,12 @@
 package ui;
 
+import com.intellij.openapi.ui.Messages;
 import graphing.Box;
+import graphing.Field;
+import graphing.Method;
 import graphing.UMLMap;
 import javax.swing.*;
-import graphing.UMLMap.Node;
+import graphing.Box.Edge;
 
 import guru.nidi.graphviz.attribute.*;
 import guru.nidi.graphviz.engine.*;
@@ -11,45 +14,85 @@ import guru.nidi.graphviz.model.*;
 import static guru.nidi.graphviz.attribute.Records.*;
 import static guru.nidi.graphviz.model.Compass.*;
 import static guru.nidi.graphviz.model.Factory.*;
+import static guru.nidi.graphviz.attribute.Records.*;
+import static guru.nidi.graphviz.model.Compass.*;
+import guru.nidi.graphviz.model.Node;
+
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class UMLDiagramPanel extends JPanel {
 
    public MutableGraph g = mutGraph("example").setDirected(false);
-
+   public Graph graph = graph("Diagram").directed();
+   public Set<Node> graphNodeSet = new HashSet<>();
    private UMLMap map;
 
-   public UMLDiagramPanel(UMLMap map) throws Exception {
+   public UMLDiagramPanel(UMLMap map) {
       this.map = map;
 
-      ArrayList<guru.nidi.graphviz.model.Node> graphNodeList = new ArrayList<>();
+      for (Map.Entry<String, Box> entry : map.getEntrySet()) {
+         Box fileClass = entry.getValue();
+         Node graphNode = createNode(fileClass);
+         graphNodeSet.add(graphNode);
 
-      for (Map.Entry<graphing.Box, ArrayList<Node>> entry : map.getEntrySet()) {
-         Box fileClass = entry.getKey();
-         ArrayList<Node> attributes = entry.getValue();
-         int index = 0;
-
-         guru.nidi.graphviz.model.Node graphNode = node(fileClass.getName())
-                 .with(Shape.BOX, Label.html("<b>fileClass.getName()</b>"));
-
-         graphNodeList.add(graphNode);
+         // Handle connection
+         for (Edge edge : fileClass.getAdjList()) {
+             graphNode.link(findNode(edge.getBox()));
+         }
       }
 
-      for (guru.nidi.graphviz.model.Node graphNode : graphNodeList) {
-         g.add(graphNode);
+      for (Node graphNode : graphNodeSet) {
+         graph.with(graphNode);
       }
 
-      Graphviz.fromGraph(g).width(200).render(Format.PNG).toFile(new File("diagrams/diagram.png"));
+      try {
+         Graphviz.fromGraph(graph).width(720).render(Format.PNG).toFile(new File("diagrams/diagram.png"));
+      } catch (IOException e) {
+         Messages.showInfoMessage(e.getMessage(), "Error");
+      }
 
       /*
       * I will call this method from UMLMap.
       * Put code to generate the diagram and add the UML diagram here.  Last
       * line should call PluginToolWindowContentPanel.addDiagramPanel(this);
       * */
+
       PluginToolWindowContentPanel.addDiagramPanel(this);
+   }
+
+   public Node createNode(Box fileClass) {
+      // Initiate CLass Name
+      String className = fileClass.getBoxType() + "\n" + fileClass.getName();
+
+      // Handle attributes
+      StringBuilder attributes = new StringBuilder();
+
+      for (Field attr : fileClass.getFields()) {
+         attributes.append(attr.toString());
+      }
+
+      // Handle Methods
+      StringBuilder methods = new StringBuilder();
+      for (Method method : fileClass.getMethods()){
+         methods.append(method.toString());
+      }
+
+      return node(className).with(Shape.BOX, Records.of(turn(rec(className), rec(attributes + "\n" + methods))));
+
+   }
+
+   public Node findNode(Box fileClass) {
+      if (!graphNodeSet.contains(node(fileClass.getName()))) {
+         return createNode(fileClass);
+      } else {
+         return node(fileClass.getName());
+      }
    }
 
 }
